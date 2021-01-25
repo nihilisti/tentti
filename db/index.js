0662aaed84041bb9fce9c420775defe9c45310d4
nihilisti <annituulial@gmail.com>
2021-01-25 13:55:35 +0200

    second commit

--- db/index.js
+++ db/index.js
@@ -1,13 +1,36 @@
 const { Pool } = require('pg')
+const { connect } = require('..')
 // const pool = new Pool()
 
-const pool = new Pool({
+// const pool = new Pool({
+//     user: 'postgres',
+//     host: 'localhost',
+//     database: 'tenttikanta',
+//     password: 'vaahter1',
+//     port: 5432,
+//   })
+
+var connectInfo = {}
+
+if (process.env.HEROKU) {
+  connectInfo = {
+    user: '',
+    host: '',
+    database: '',
+    password: '',
+    port: ''
+  }
+} else {
+  connectInfo = {
     user: 'postgres',
     host: 'localhost',
     database: 'tenttikanta',
     password: 'vaahter1',
-    port: 5432,
-  })
+    port: 5432
+  }
+}
+
+const pool = new Pool(connectInfo)
 
 module.exports = {
   query: (text, params, callback) => {
